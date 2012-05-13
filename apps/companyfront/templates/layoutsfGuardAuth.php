a81590d7c6f0c58c708047166be6a77e02230eee
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2012-05-13 04:15:25 +0200

    Layout for login page.
    I am using sfDoctrineGuardPlugin with a custom layout.

---
+++ apps/companyfront/templates/layoutsfGuardAuth.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <title>Mobi - Mobile Ads</title>
+    <?php include_stylesheets() ?>
+  </head>
+  <body>
+  <div id="main_container">
+	<div class="header_login">
+    	<div class="logo"><a href="#"><img src="" alt="" title="" border="0" /></a></div>
+    </div>
+    <div class="login_form">
+		<?php echo $sf_content ?>
+    </div>  
+	<div class="footer_login">
+        <div class="left_footer_login">MOBI ADS | Powered by <a href="http://uah.es">UAH</a></div>
+        <div class="right_footer_login"><a href="http://uah.es"><img src="" alt="" title="" border="0" /></a></div>
+    </div>
+   </div>		
+   </body>
+</html>
