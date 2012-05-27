a1801688bd7b31036dfcb5efa768a51343506d73
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2012-05-27 22:36:39 +0200

    Companies: register web page with RECAPTCHA

---
+++ apps/companyfront/modules/register/actions/actions.class.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * api actions.
+ *
+ * @package    mobileadvertising
+ * @subpackage api
+ * @author     Gustavo Martin Morcuende
+ * @version
+ */
+class registerActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    if ($this->getUser()->isAuthenticated())
+    {
+      $this->getUser()->setFlash('notice', 'You are already registered and signed in!');
+      $this->redirect('@homepage');
+    }
+
+
+    $this->form = new CompaniesRegisterForm();
+
+    if ($request->isMethod('post'))
+    {
+      $captcha = array('recaptcha_challenge_field' => $request->getParameter('recaptcha_challenge_field'),
+                       'recaptcha_response_field'  => $request->getParameter('recaptcha_response_field'),
+                      );
+      $this->form->bind(array_merge($request->getParameter($this->form->getName()), array('captcha' => $captcha)));
+      if ($this->form->isValid())
+      {
+        $user = $this->form->save();
+        $user->addPermissionByName('companies');
+        $this->getUser()->signIn($user);
+
+        $this->redirect('@homepage');
+      }
+    }
+  }
+}
---
+++ lib/form/doctrine/CompaniesRegisterForm.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * CompaniesRegisterForm for registering new users
+ *
+ * @package    mobileadvertising
+ * @subpackage form
+ * @author     Gustavo Martin Morcuende
+ * @version
+ */
+class CompaniesRegisterForm extends UsersRegisterForm
+{
+  /**
+   * @see sfForm
+   */
+  public function configure()
+  { 
+    //Company creation form
+    $company = new Company();
+    $company->User = $this->getObject();
+    $newCompanyForm = new CompanyForm($company);
+
+    $this->embedForm('new', $newCompanyForm);
+
+    parent::configure();
+  }   
+} 
