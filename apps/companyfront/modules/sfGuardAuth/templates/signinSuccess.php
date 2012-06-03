9e47a04a42978788d048a06834da588931bbb602
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2012-06-03 19:21:07 +0200

    Companies may just register through one admin.

--- apps/companyfront/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/companyfront/modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,4 +1,4 @@
 <?php use_helper('I18N') ?>
-<h1><?php echo __('Log In or Register', null, 'sf_guard') ?></h1>
+<h1><?php echo __('Log In', null, 'sf_guard') ?></h1>
 
 <?php include_partial('signin_form', array('form' => $form)) ?>
