21464c8e3941d5d863701914a5f884f5d4795008
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2012-06-10 23:22:46 +0200

    Web Service GetAdsbyGPS returns ad name.

--- apps/userfront/modules/api/templates/getadsbygpsSuccess.json.php
+++ apps/userfront/modules/api/templates/getadsbygpsSuccess.json.php
@@ -1,6 +1,6 @@
 [
 <?php foreach ($ads as $ad): ?>
   { "id" : "<?php echo $ad->getAd()->getId()?>", "image" : "<?php echo $ad->getAd()->getAdMobileImageLink() ?>", "link" : "<?php echo $ad->getAdLink()?>",
-    "text" : "<?php echo $ad->getAdMobileText()?>" },
+    "text" : "<?php echo $ad->getAdMobileText()?>", "adname" : "<?php echo $ad->getAdName()?>" },
 <?php endforeach ?>
 ]
